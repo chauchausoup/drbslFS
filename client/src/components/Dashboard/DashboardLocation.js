0dfc6470abe900275e88728e057f005d3bbe5056
chauchausoup <7krishna7dhakal7@gmail.com>
2020-07-15 12:20:01 +0545

    need to work on maps

--- client/src/components/Dashboard/DashboardLocation.js
+++ client/src/components/Dashboard/DashboardLocation.js
@@ -1,19 +1,92 @@
-import React from 'react';
 
+import React, { Component } from "react";
+import { Map, TileLayer, Marker } from "react-leaflet";
 
-function DashboardLocation(){
-return(
-      <div id="locaiton_dashboard">
-      <h3>
-            Postal Code of your current location:
-      </h3>
-            <input type="number" name="postalCode" id="postal_code"/><br/>
+export default class DashboardLocation extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.comRef=React.createRef();
+    
+    this.state = {
+      center: {
+        lat: 28.2,
+        lng: 83.99,
+      },
+      zoom: 15,
+      draggable: true,
+      markerData: [],
+    }
+  }
+  
+ 
 
-            <button type="submit">Submit</button>
-      </div>
-)
+  //add marker
+
+  addMarker = (event) => {
+    const coords = event.latlng;
+    this.setState({
+      markerData: [...this.state.markerData, coords],
+    });
+    console.log(this.state.markerData);
+  };
+
+  //remove marker
+
+  removeMarkerHandler = (event) => {
+    
+    const coords = event.latlng;
+    
+
+    this.state.markerData.splice(this.state.markerData.indexOf(coords),1)
+    this.setState({
+              markerData:[...this.state.markerData]
+        })  
+  };
+
+  //remove actual marker
+
+  removeMarker=()=>{
+   
+     
+  }
+
+  //update marker
+
+  updateMarker = (event) => {
+    console.log(this.state.markerData);
+    const latLng = event.target.getLatLng(); //get marker LatLng
+    const markerIndex = event.target.options.marker_index; //get marker index
+    //update
+    this.setState((prevState) => {
+      const markerData = [...prevState.markerData];
+      markerData[markerIndex] = latLng;
+      return { markerData: markerData };
+    });
+  };
+
+  render() {
+    return (
+      <Map
+        center={this.state.center}
+        zoom={this.state.zoom}
+        onClick={this.addMarker}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {this.state.markerData.map((element, index) => (
+          <Marker
+            key={index}
+            marker_index={index}
+            position={element}
+            draggable={this.state.draggable}
+            onDragend={this.updateMarker}
+           onclick={this.removeMarkerHandler}
+         />
+        ))}
+      </Map>
+    );
+  }
 }
-
-export default DashboardLocation;
-
-//some changes in client
