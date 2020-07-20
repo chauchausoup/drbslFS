8fe65a9a677780be9cbb18b1acb32090f5b1ffa5
chauchausoup <7krishna7dhakal7@gmail.com>
2020-07-20 18:19:48 +0545

    failed attemp to the leaflet

--- client/src/components/Dashboard/DashboardLocation.js
+++ client/src/components/Dashboard/DashboardLocation.js
@@ -4,14 +4,123 @@ import '../../App.css'
 import L from 'leaflet';
 
 
-function DashboardLocation({markerPosition}){
+function DashboardLocation(){
+
+
+
+const COORDI = [28.4, 83.99];
+
+const sampleMarkerText = [
+
+  `<h3>Hospital 1</h3><br>
+     <p>This is hospital 1.</p><br>`
+  ,
+
+  `<h1>Hospital 1</h1><br>
+ 
+     <p>This is hospital 1.</p><br>
+     
+     <button>Hey</button>`
+
+];
+
+var popupLayout = sampleMarkerText[0];
+
+
+function roundNumber(number, tensplace = 10) {
+  return Math.round(number * tensplace) / tensplace;
+}
 
     //lets create a map
 
+const markerRef = useRef(null);
+const markerGroupRef = useRef(null)
 const mapRef = useRef(null);
+//const popupLayoutRef = useRef(null);
 
-  useEffect(()=>{
+var markers = [];
 
+var mapOptions = {
+  center: COORDI,
+  zoom: 13,
+  dragging: true,
+}
+
+var markerOptions = {
+  removable: true,
+  editable: true,
+  maxWidth: 750,
+  autoPan: false
+};
+
+
+
+useEffect(()=>{
+
+//Create a map and assign it to the map div
+
+mapRef.current = L.map('leafletMapid', mapOptions);
+console.log(mapRef.current)
+
+ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Tiles &copy; Esri &mdash; Sources: OpenStreetMaps',
+  maxZoom: 18,
+}).addTo(mapRef.current);
+
+L.marker(COORDI).addTo(mapRef.current)
+  .bindPopup(popupLayout, markerOptions)
+  .openPopup()
+
+},[]);
+
+markerGroupRef.current = new L.LayerGroup();
+console.log(mapRef.current)
+
+
+function addMarker(e) {
+  // Add marker to map at click location; add popup window
+ markerRef.current = new L.marker(e.latlng)
+    .addTo(mapRef.current)
+    .bindPopup(popupLayout, markerOptions)
+    .openPopup();
+  markers.push(markerRef.current);
+
+  // i need a sample marker text generator here..
+
+  // sampleMarkerTex(newMarker)
+
+}
+
+function getFeaturesInView() {
+  var features = [];
+  mapRef.current.eachLayer(function (layer) {
+    if (layer instanceof L.Marker) {
+
+      if (mapRef.current.getBounds().contains(layer.getLatLng())) {
+
+        features.push({
+          coordinates: layer._latlng,
+          popupContent: layer._popup._content
+        });
+
+
+      }
+    }
+  });
+  return features;
+}
+
+const submitHandler=(e)=>{
+  e.preventDefault();
+ markerGroupRef.current.clearLayers();  
+  var feat = getFeaturesInView();
+
+  console.log(feat);
+
+}
+
+/*   useEffect(()=>{
+    
    mapRef.current = L.map('mapid',{
       center:[28.2,83.99],
       zoom:16,
@@ -24,7 +133,9 @@ const mapRef = useRef(null);
     })
 
   },[]);
+ */
 
+/* 
   //lets add a marker
   const markerRef = useRef(null);
 
@@ -39,40 +150,35 @@ const mapRef = useRef(null);
                           .openPopup()
     }
 
-  },[markerPosition])
+  },[markerPosition]) */
+
 
 
   
   return(
-    <div id="mapid">
-      
-    </div>
+    <>
+      <div id="leafletMapid" className="mapdiv">
+
+      </div>
+      <div className="controlButtons">
+         <button type="button" name="button" id="submitMap" onClick={submitHandler}>Submit</button>
+      </div>
+      </>
   )
 
 }
 
 function DashboardLocation2(){
-  const[markerPosition,setMarkerPosition]= useState({
-    lat:28.2,
-    lng:83.99
-  });
-  const {lat,lng}= markerPosition;
 
-  function moveMarker() {
-    setMarkerPosition({
-      lat: lat + 0.0001,
-      lng: lng + 0.0001
-    });
-  }
 
   return(
     <div>
-      <DashboardLocation markerPosition={markerPosition}/>
-      {lat}
+      <DashboardLocation/>
+      {/* {lat}
       <br/>
       {lng}
       <br/>
-      <button onClick={moveMarker}>Move Marker</button>
+      <button onClick={moveMarker}>Move Marker</button> */}
     </div>
   )
 }
